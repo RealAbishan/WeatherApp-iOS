6cdd7480d103530f53c9e9125fb2331fda735de3
Abishan <highoncode09@gmail.com>
2023-02-27 19:46:04 +0530

    Initial Commit

---
+++ WeatherApp-iOS/WeatherApp_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_iOSApp.swift
+//  WeatherApp-iOS
+//
+//  Created by Abishan Parameswaran on 2023-02-27.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
